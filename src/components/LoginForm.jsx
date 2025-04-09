ec06a3aeb5f9fe586980755008bcfafd2fc9626d
Elsie <elsieoscar06@gmail.com>
2025-04-09 21:12:23 +0300

    linkedin

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,1 +1,30 @@
+import React from "react";
 
+function LoginForm() {
+  return (
+    <>
+      <form>
+        <div className="container">
+          <label htmlFor="uname">UserName:</label><br/>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+            required
+          ></input><br></br>
+
+          <label htmlFor="psw">Password:</label><br/>
+          <input
+            type="text"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          ></input><br></br>
+
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </>
+  );
+}
+export default LoginForm;
---
+++ src/components/LoginHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoginHeader(){
+    return(
+        <>
+        <h1>Welcome To Linkedin</h1>
+        <p>Please enter your details below</p>
+        </>
+        
+    )
+}
+export default LoginHeader
